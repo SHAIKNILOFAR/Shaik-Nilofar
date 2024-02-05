74ec91076d9fe591f1fc34a4fdd58f2ec72e0703
SHAIKNILOFAR <159048540+SHAIKNILOFAR@users.noreply.github.com>
2024-02-05 21:36:28 +0530

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,7 @@
+a=int(input("enetr the number_1:"))
+b=int(input("enter the number_2:"))
+if(type(a)=='int'or'float')and(type(b)=='int'or'float'):
+	div=a/b
+	print("the division is",div)
+else:
+	print("the number are not the real numbers")
